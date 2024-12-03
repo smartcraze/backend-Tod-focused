67877cf1adf00308c93f3cfeb2622d71a1ed1d4c
smartcraze <surajkv9973@gmail.com>
2024-12-04 00:07:20 +0530

    ready to put in vercel

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import express from 'express';
 import dotenv from 'dotenv';
 import attendanceRoutes from './routes/attendanceRoutes.js';
 import cors from 'cors';
+
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 8000;
 
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Attendance API!');
@@ -15,6 +15,4 @@ app.get('/', (req, res) => {
 
 app.use('/api', attendanceRoutes);
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+export default app;
