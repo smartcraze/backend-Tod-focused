ead6888932710fe63f54bcaf58ba54d77fe2858e
smartcraze <surajkv9973@gmail.com>
2024-12-08 02:48:25 +0530

    done

--- index.js
+++ index.js
@@ -2,22 +2,31 @@ import express from 'express';
 import dotenv from 'dotenv';
 import AllRoutes from './routes/attendanceRoutes.js';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
+
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Attendance API!');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// API routes
 app.use('/api', AllRoutes);
 
-// app.listen(3000,()=>{
-//   console.log("App is listenign on 3000");
-  
-// })
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`);
+});
 
 export default app;
